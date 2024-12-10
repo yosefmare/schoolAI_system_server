aa8effe3d756b9ebc9d97f5936b5af0fdcd94f56
Yosef Mare <yosefmare108@gmail.com>
2024-12-10 11:56:03 +0200

    update DB config and deletec unused file

--- config/connectDB.js
+++ config/connectDB.js
@@ -5,7 +5,7 @@ import { connect } from "mongoose";
 // Connect to MongoDB asynchronously in function
 async function connectDB() {
   try {
-    await connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/school_ai_system" );
+    await connect(process.env.MONGODB_URI );
     console.log("MongoDB connected...");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error.message);
